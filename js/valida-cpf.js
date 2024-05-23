1f397d43e67c41828920b1af45bb780f39aac6ca
Marlinton Aldrighi <marlinton13@gmail.com>
2024-05-23 16:25:35 -0300

    Third class

--- js/valida-cpf.js
+++ js/valida-cpf.js
@@ -1,6 +1,11 @@
 export default function ehUmCPF(campo){
     const cpf = campo.value.replace(/\.|-/g, "")
-    console.log(validaNumerosRepetidos(cpf));
+    validaPrimeiroDigito(cpf)
+    if(validaNumerosRepetidos(cpf) || validaPrimeiroDigito(cpf) || validaSegundoDigito(cpf)){
+        console.log("Este CPF não existe!");
+    }else{
+        console.log("Este CPF é válido");
+    }
 }
 
 function validaNumerosRepetidos(cpf){
@@ -17,4 +22,40 @@ function validaNumerosRepetidos(cpf){
         '99999999999'    
     ]
     return numerosRepetidos.includes(cpf)
+}
+
+function validaPrimeiroDigito(cpf){
+    let soma = 0
+    let multiplicador = 10
+
+    for(let tamanho = 0; tamanho < 9; tamanho++){
+        soma += cpf[tamanho] * multiplicador
+        multiplicador--
+    }
+
+    soma = (soma * 10) % 11
+
+    if(soma == 10 || soma == 11){
+        soma = 0
+    }
+
+    return soma != cpf[9]
+}
+
+function validaSegundoDigito(cpf){
+    let soma = 0
+    let multiplicador = 11
+
+    for(let tamanho = 0; tamanho < 10; tamanho++){
+        soma += cpf[tamanho] * multiplicador
+        multiplicador--
+    }
+
+    soma = (soma * 10) % 11
+
+    if(soma == 10 || soma == 11){
+        soma = 0
+    }
+
+    return soma != cpf[10]
 }
